fae131f5f5210a3f346fe2b13b915cd9d6370fdf
Oguntoyinbo Boluwatife <81614588+Teepheh-Git@users.noreply.github.com>
2022-02-01 22:48:09 +0100

    progress

--- src/screens/paymentWebPage/index.js
+++ src/screens/paymentWebPage/index.js
@@ -1,16 +1,70 @@
 // @flow
 import React, {useContext} from 'react';
-import {View, StyleSheet} from "react-native";
+import {StyleSheet, View} from "react-native";
 import {Paystack} from "react-native-paystack-webview/lib";
 import {COLORS} from "../../constants";
 import BackButton from "../../components/BackButton";
 import {UserContext} from "../../context/UserContext";
+import {handleQuery} from "../../graphql/requests";
 
 const PaymentWebPage = ({navigation, route}) => {
 
     const user = useContext(UserContext)
 
-    const amount = Number(route.params)
+    const amount = parseFloat(route.params.replace(",", ""))
+    // console.log(amount, " amount")
+
+
+    const TransactionData = async (res) => {
+
+        let qry = `mutation {
+                createSavingsTransaction(
+                input: {
+                    data: {
+                    reference: "${res.transactionRef.trxref}"
+                    amount_paid: ${amount}
+                    status: ${res.status.toUpperCase()}
+                    user_id: ${user.id} 
+                    community_id: 15
+            
+
+                }
+                }
+            ) {
+                    savingsTransaction {
+                        id
+                        user_id {
+                                id
+      }
+    }
+  }
+}`
+
+
+        try {
+
+            // console.log(qry, "QRYYYY")
+
+            let qryRes = await handleQuery(qry, user.token, false)
+            // console.log(qryRes, "QRYRESSS")
+
+
+            if (res.status){
+                navigation.navigate("SuccessScreen")
+            }
+
+
+            // console.log(res.data.event, "REZZZZ")
+            // console.log(res.status, "Statussss")
+            // console.log(res.transactionRef.trxref, "trxreffff")
+
+        } catch (e) {
+
+            console.log(e, "ERRORtrx")
+
+
+        }
+    }
 
 
     return (
@@ -18,16 +72,17 @@ const PaymentWebPage = ({navigation, route}) => {
             <BackButton onPress={() => navigation.pop()}/>
             <Paystack
                 paystackKey="pk_test_b0f9c1e94ea37b2ba7df563002ee8a074bc5678a"
-                amount={"10000"}
+                amount={amount}
                 billingEmail={user.email}
                 activityIndicatorColor={COLORS.primary}
                 onCancel={(e) => {
                     // handle response here
                     console.log(e, "PaymentError")
                 }}
-                onSuccess={(res) => {
+                // channels={JSON.stringify(["card","ussd"])}
+                onSuccess={async (res) => {
+                    await TransactionData(res)
                     // handle response here
-                    console.log(res, "PaymentSuccess")
                 }}
                 autoStart={true}
             />
